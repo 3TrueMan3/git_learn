0269f0d8beebe6b26b6ac973acdcb21cf214d1fb
Michael <truemisha90@gmail.com>
2019-04-22 22:58:24 +0300

    Там не так всё плохо оказывается, загляни

--- 1.py
+++ 1.py
@@ -3,3 +3,4 @@ import pandas as pd
 arr = [[1, 2, 3, 4], [5, 6, 7, 8]]
 arr = pd.DataFrame(arr, index=[2016, 2017], columns=['opel', 'saab', 'toyota', 'lada'])
 print(arr)
+print("А здесь не так то плохо:)")
