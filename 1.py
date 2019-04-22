3fcdd68e719b57e208e835aae460ea94e03c728e
Michael <truemisha90@gmail.com>
2019-04-22 22:47:22 +0300

    Твоё будущее пока туманно

--- 1.py
+++ 1.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 arr = pd.DataFrame(arr, index=[2016, 2017], columns=['opel', 'saab', 'toyota', 'lada'])
-print('Это всё ради твоего будущего')
+print('Это всё ради твоего будущего, а оно пока туманно')
 print(arr)
