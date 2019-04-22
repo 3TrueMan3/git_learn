a7f3d78e3d5951154b2bfcc45d52bd93fa76d7dd
Michael <truemisha90@gmail.com>
2019-04-22 22:46:00 +0300

    Это ради твоего будущего

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-arr = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
 arr = pd.DataFrame(arr, index=[2016, 2017], columns=['opel', 'saab', 'toyota', 'lada'])
+print('Это всё ради твоего будущего')
 print(arr)
